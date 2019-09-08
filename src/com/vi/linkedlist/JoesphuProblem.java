8bf4a35fa7e355287dc25ab9c27e2444abe759ca
coVdiing <113714806@qq.com>
2019-09-09 07:18:37 +0800

    完成了joesphuProblem(int startNo,int totalNumber,int pace)方法，约瑟夫问题的具体步骤

--- src/com/vi/linkedlist/JoesphuProblem.java
+++ src/com/vi/linkedlist/JoesphuProblem.java
@@ -9,8 +9,10 @@ package com.vi.linkedlist;
 public class JoesphuProblem {
 	public static void main(String[] args) {
 		JoesphuList jl = new JoesphuList();
-		jl.addNode(25);
-		jl.showList();
+//		jl.addNode(25);
+//		jl.showList();
+		//测试约瑟夫环游戏
+		jl.joesphuProblem(10,125,20);
 	}
 }
 
@@ -68,6 +70,48 @@ class JoesphuList {
 			System.out.printf("娃儿的号码是:%d\n", cur.getNo());
 			cur = cur.getNext();
 		}
+	}
+	
+	/**
+	 * 约瑟夫环问题，娃儿报数出圈
+	 * @param startNo 开始序号
+	 * @param number 娃儿的总数
+	 * @param pace 每次第几个娃儿出圈
+	 */
+	public void joesphuProblem(int startNo,int number,int pace){
+		//对数据做一个判断
+		if(startNo > number || pace < 1 ) {
+			System.out.println("数据有问题撒!");
+		}
+		//先生成指定number的环
+		addNode(number);
+		//定义一个辅助指针，用于指向遍历指针的前一个节点
+		BoyNode helper = first;
+		while(helper.getNext() != first) {
+			helper = helper.getNext();
+		}
+		//让first指针移动到startNo这个娃儿这里
+		for(int i = 0 ; i < startNo - 1;i++) {
+			first = first.getNext();
+			helper = helper.getNext();
+		}
+		System.out.println("link start!\n=================================");
+		//开始游戏
+		while(true) {
+			if(helper == first)
+				break;
+			for(int i = 0 ; i < pace - 1; i++) {
+				//开始移动两个指针
+				first = first.getNext();
+				helper = helper.getNext();
+			}
+			System.out.println("娃儿["+first.getNo()+"]退出了连接!");
+			first = first.getNext();
+			helper.setNext(first);
+		}
+		System.out.println("最后留下的幸运儿是娃儿["+first.getNo()+"]!奖励一辆乔碧萝tank");
+		System.out.println("=================================");
+		System.out.println("game over!");
 	}
 }
 
